65fdf792e36124d383aff862e357c59c7d819d23
cabinboy1031 <hemmerian@gmail.com>
2019-02-10 16:50:37 -0800

    Implementation of initial Vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <vulkan/vulkan.h>
 
-using namespace std;
 
 int main() {
-    cout << "Hello world~!" << endl;
+    VkInstance inst;
+    VkResult res;
+    VkInstanceCreateInfo instInfo;
+    instInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instInfo.pNext = NULL;
+    instInfo.flags = 0;
+    instInfo.pApplicationInfo = NULL;
+    instInfo.enabledLayerCount = 0;
+    instInfo.ppEnabledLayerNames = NULL;
+    instInfo.enabledExtensionCount = 0;
+    instInfo.ppEnabledExtensionNames = 0;
+
+    res = vkCreateInstance(&instInfo, NULL, &inst);
+    if (res == VK_ERROR_INCOMPATIBLE_DRIVER) {
+        std::cout << "cannot find a compatible Vulkan ICD\n";
+        exit(-1);
+    } else if (res) {
+        std::cout << "unknown error\n";
+        exit(-1);
+    }
+
     return 0;
 }
